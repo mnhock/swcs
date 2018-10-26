1eb27f58f053db87e5a52feff78b3f14c7cbbd59
mnhock <Martin.Hock@gfk.com>
2018-10-26 11:07:13 +0200

    Clean up

--- swcs-compression/src/main/java/swcs/converter/before/Converter.java
+++ swcs-compression/src/main/java/swcs/converter/before/Converter.java
@@ -5,24 +5,24 @@ import java.io.File;
 public class Converter {
 
     public static File convert(String format, File file) {
-        System.out.println("CompressionFacade: convert started.");
+        System.out.println("Compression: convert started.");
 
         // Check if source format supported
 
-        File file2 = null;
+        File compressedFile = null;
 
         if (format.equals("png")) {
-            System.out.println("CompressionFactory: png compression");
+            System.out.println("Compression: png compression");
             // do compression on file;
             // compressedFile =
         } else if (format.equals("jpg")) {
-            System.out.println("CompressionFactory: jpg compression");
+            System.out.println("Compression: jpg compression");
             // do compression on file;
             // compressedFile =
         }
 
-        System.out.println("CompressionFacade: convert completed.");
+        System.out.println("Compression: convert completed.");
 
-        return file2;
+        return compressedFile;
     }
 }
