92e4e1b2de4e79594860498f88b7345a38b24577
mnhock <Martin.Hock@gfk.com>
2020-10-16 14:27:56 +0200

    Add exercise for ImmutableCar

---
+++ swcs-clean/src/main/java/swcs/clean/before/ImmutableCar.java
@@ -0,0 +1,33 @@
+package swcs.clean.before;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ImmutableCar {
+
+    private String vin;
+    private Date manufactored;
+    private ArrayList previousOwners = new ArrayList();
+
+    public ImmutableCar(String number, Date manufactored) {
+        this.vin = number;
+        this.manufactored = manufactored;
+    }
+
+    public Date getManufactored() {
+        return this.manufactored;
+    }
+
+    public String getVin() {
+        return this.vin;
+    }
+
+    public ArrayList getPreviousOwners() {
+        return this.previousOwners;
+    }
+
+    public boolean addPreviousOwner(String text) {
+        text.trim();
+        return this.previousOwners.add(text);
+    }
+}
