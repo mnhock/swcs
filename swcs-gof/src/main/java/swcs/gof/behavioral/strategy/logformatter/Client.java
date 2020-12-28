69965e2d4e83479bed653cd65193c76929ee0b47
mnhock <Martin.Hock@gfk.com>
2020-12-28 20:56:42 +0100

    Clean up

--- swcs-gof/src/main/java/swcs/gof/behavioral/strategy/logformatter/Client.java
+++ swcs-gof/src/main/java/swcs/gof/behavioral/strategy/logformatter/Client.java
@@ -7,7 +7,11 @@ public class Client {
 
     public static void main(String[] args) {
         GitConsole console = new GitConsole(new FullLogFormatter());
-        console.printLog(List.of(new Log("3976f3a2b1b258afead13e15a45cce53cd99c670", "cleancoder", "Clean up", LocalDateTime.now()),
-                new Log("07aa35b3488abd589e9a42a3b1c0176b82b3a4d5", "cleancoder", "Fix NPE in LogFormatter", LocalDateTime.now())));
+        
+        List<Log> logs = List.of(
+                new Log("397c670", "cleancoder", "Clean up", LocalDateTime.now()),
+                new Log("07aa4d5", "cleancoder", "Fix NPE in Log", LocalDateTime.now()));
+        
+        console.printLog(logs);
     }
 }
