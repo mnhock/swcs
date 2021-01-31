8c46bc6d8f33216e1b7109172da747c2328206a7
mnhock <Martin.Hock@gfk.com>
2021-01-31 19:33:36 +0100

    Add missing classes

---
+++ swcs-dp/src/main/java/swcs/dp/isp/techemployee/after/SecurityTester.java
@@ -0,0 +1,10 @@
+package swcs.dp.isp.techemployee.after;
+
+public class SecurityTester implements Tester {
+
+    @Override
+    public void test() {
+        System.out.println("Yes, I love it!");
+    }
+
+}
---
+++ swcs-dp/src/main/java/swcs/dp/isp/techemployee/after/UIDesigner.java
@@ -0,0 +1,10 @@
+package swcs.dp.isp.techemployee.after;
+
+public class UIDesigner implements Designer {
+
+    @Override
+    public void design() {
+        System.out.println("Yes, I love it!");
+    }
+
+}
