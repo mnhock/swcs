ac7d602247c8523ce4d68e59cf0ccd6c7a880d55
mnhock <Martin.Hock@gfk.com>
2021-11-08 20:13:16 +0100

    Wrap tests with assertAll

--- swcs-junit/src/test/java/swcs/junit/converter/FahrenheitCelsiusConverterTest.java
+++ swcs-junit/src/test/java/swcs/junit/converter/FahrenheitCelsiusConverterTest.java
@@ -1,5 +1,6 @@
 package swcs.junit.converter;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -8,15 +9,20 @@ class FahrenheitCelsiusConverterTest {
 
     @Test
     void shouldConvertCelsiusToFahrenheit() {
-        assertEquals(32, FahrenheitCelsiusConverter.toFahrenheit(0));
-        assertEquals(98, FahrenheitCelsiusConverter.toFahrenheit(37));
-        assertEquals(212, FahrenheitCelsiusConverter.toFahrenheit(100));
+        assertAll(
+                () -> assertEquals(32, FahrenheitCelsiusConverter.toFahrenheit(0)),
+                () -> assertEquals(98, FahrenheitCelsiusConverter.toFahrenheit(37)),
+                () -> assertEquals(212, FahrenheitCelsiusConverter.toFahrenheit(100))
+        );
+
     }
 
     @Test
     void shouldConvertFahrenheitToCelsius() {
-        assertEquals(0, FahrenheitCelsiusConverter.toCelsius(32));
-        assertEquals(37, FahrenheitCelsiusConverter.toCelsius(100));
-        assertEquals(100, FahrenheitCelsiusConverter.toCelsius(212));
+        assertAll(
+                () -> assertEquals(0, FahrenheitCelsiusConverter.toCelsius(32)),
+                () -> assertEquals(37, FahrenheitCelsiusConverter.toCelsius(100)),
+                () -> assertEquals(100, FahrenheitCelsiusConverter.toCelsius(212))
+        );
     }
 }
