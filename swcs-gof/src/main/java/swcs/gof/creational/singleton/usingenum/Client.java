fd377c9244f0445f21626a8d4161161950b017b4
mnhock <Martin.Hock@gfk.com>
2022-05-03 09:16:26 +0200

    Clean up

--- swcs-gof/src/main/java/swcs/gof/creational/singleton/usingenum/Client.java
+++ swcs-gof/src/main/java/swcs/gof/creational/singleton/usingenum/Client.java
@@ -3,7 +3,7 @@ package swcs.gof.creational.singleton.usingenum;
 public class Client {
 
     public static void main(String[] args) {
-        System.out.println(Singleton.INSTANCE.equals(Singleton.INSTANCE));
+        System.out.println(Singleton.INSTANCE == Singleton.INSTANCE);
 
     }
 }
