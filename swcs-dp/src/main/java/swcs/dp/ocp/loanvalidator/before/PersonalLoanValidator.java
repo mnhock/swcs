f89f14f6631c0dd7046565b5291f9de5e4849e60
mnhock <Martin.Hock@gfk.com>
2022-10-22 12:46:28 +0200

    Change to final

--- swcs-dp/src/main/java/swcs/dp/ocp/loanvalidator/before/PersonalLoanValidator.java
+++ swcs-dp/src/main/java/swcs/dp/ocp/loanvalidator/before/PersonalLoanValidator.java
@@ -1,6 +1,6 @@
 package swcs.dp.ocp.loanvalidator.before;
 
-public class PersonalLoanValidator {
+public final class PersonalLoanValidator {
 
     public boolean isValid(int balance) {
         return balance > 1000;
